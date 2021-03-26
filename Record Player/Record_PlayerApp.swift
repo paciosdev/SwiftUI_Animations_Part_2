24bd6b9e9299fb41e57e177571b2b1c2c7c5b749
Francesco Paciello <kekkopacio@gmail.com>
2021-03-26 18:02:50 +0100

    Initial Commit

---
+++ Record Player/Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_PlayerApp.swift
+//  Record Player
+//
+//  Created by Kekko Paciello on 26/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
